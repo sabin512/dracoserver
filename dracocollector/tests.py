9e84010dedc754a469ac77301408d6b0b8fb50f3
Sabin <sabin@raspberrypi.(none)>
2015-08-24 00:00:46 -0400

    Start adding unit tests

--- dracocollector/tests.py
+++ dracocollector/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.core.urlresolvers import reverse
 
-# Create your tests here.
+
+class ReadingCollectorTests(TestCase):
+    def test_reading_collector_missing_source(self):
+        response = self.client.get(reverse('collector'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'sourceName')
+    
+    def test_reading_collector(self):
+        response = self.client.get(reverse('collector'),{'sourceName': 'unitTestSource',
+                                                         'temperatureReading': '56',
+                                                         'humidityReading': '44'})
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Received sensor reading from unitTestSource')
